d66924e008c660a28e567866a183775a1df46c66
Kiran Kumar Pradhan <pradhankukiran@gmail.com>
2026-05-09 02:10:07 +0530

    feat(sidecar): define pipeline definition + event contracts

---
+++ packages/sidecar/src/contracts.ts
@@ -0,0 +1,10 @@
+export type {
+  PipelineDefinition,
+  PipelineEventSink,
+  PipelineInput,
+  PipelineRunEvent as PipelineEvent,
+  PipelineStep,
+  PipelineStepResult,
+  PipelineStepStatus,
+  ProviderLane
+} from "@pipelinekit/core";
